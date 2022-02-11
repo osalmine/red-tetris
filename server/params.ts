f82c553ec4d4fcdbea6a3c19a37c64ed9be8eeea
Onni Salminen <o.j.salminen@gmail.com>
2022-02-11 18:45:42 +0200

    Modify params

--- server/params.ts
+++ server/params.ts
@@ -2,12 +2,12 @@ const params = {
   server: {
     host: process.env.SERVER_HOST || 'localhost',
     port: process.env.SERVER_PORT || '3004',
-    get url() { return `http://${ this.host }:${ this.port}` },
+    get url() { return `http://${ this.host }:${ this.port}`; },
   },
   board: {
     rows: 20,
     cols: 10,
   },
-}
+};
 
-export default params
+export default params;
