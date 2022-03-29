554d48f44c3f9a64f8dd087420aeaf424f224ce7
Onni Salminen <o.j.salminen@gmail.com>
2022-03-29 15:49:46 +0300

    Move piece down on ArrowDown and start new interval

--- client/src/handlers/pieceMovement.ts
+++ client/src/handlers/pieceMovement.ts
@@ -6,21 +6,16 @@ import { RootState, store } from '../store';
 
 const pieceMoveInterval = 1500;
 
-// const pieceMoveDownHandler = (dispatch: Dispatch) => {
-//   // const dispatch = useAppDispatch();
-
-//   // return setInterval(() => dispatch(movePieceDown()), 1500);
-//   const timer = setTimeout(
-//     () => dispatch(movePieceDown()),
-//     pieceMoveInterval,
-//   );
-//   return timer;
-// };
+let interval: NodeJS.Timer | null;
 
 const playerIsAdmin = (state: RootState) => {
   const adminPlayerName = state.state.players.find(player => player.isAdmin === true)?.name;
   return state.client.playerName === adminPlayerName;
 };
+
+const startPieceMoveInterval = () => setInterval(() => {
+  store.dispatch(movePieceDown());
+}, pieceMoveInterval);
 
 document.addEventListener('keydown', (e) => {
   console.log(`EVENT LISTENER event ${e.code}`);
@@ -31,17 +26,21 @@ document.addEventListener('keydown', (e) => {
       store.dispatch(startGame({ playerName, roomName }));
     }
   }
-});
 
-let interval: NodeJS.Timer | undefined;
+  if (e.code === 'ArrowDown') {
+    store.dispatch(movePieceDown());
+    if (interval) {
+      clearInterval(interval);
+    }
+    interval = startPieceMoveInterval();
+  }
+});
 
 const pieceMoveDownHandler = () => {
   const state = store.getState();
 
   if (state.state.gameState === 'playing' && !interval) {
-    interval = setInterval(() => {
-      store.dispatch(movePieceDown());
-    }, pieceMoveInterval);
+    interval = startPieceMoveInterval();
   }
   console.log('pieceMoveDownHandler');
 };
