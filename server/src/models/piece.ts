0f11bcd4f1627124ef58d2c7afee90249d976cc2
Onni Salminen <onni.salminen@wolt.com>
2023-03-18 19:00:14 +0200

    Server remove unnecessary piece constants

--- server/src/models/piece.ts
+++ server/src/models/piece.ts
@@ -1,19 +1,14 @@
-import pieces, { PieceName, pieceNames } from '../constants/pieces';
+import { PieceName, pieceNames } from '../constants/pieces';
 import { shuffle } from 'lodash';
 import debug from 'debug';
 
-const logerror = debug('tetris:error'),
-  loginfo = debug('tetris:info');
+const loginfo = debug('tetris:info');
 
 export default class Piece {
   gamePieces: PieceName[];
 
   constructor() {
     this.gamePieces = [];
-  }
-
-  addPieces(count: number) {
-    return;
   }
 
   generateBatch(): PieceName[] {
