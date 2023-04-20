d0ea53fb15e796677e9d47b5709960d3c1385f3d
Onni Salminen <onni.salminen@wolt.com>
2023-04-20 23:42:33 +0300

    Client add a new test for Pending

--- client/src/containers/Pending.spec.tsx
+++ client/src/containers/Pending.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithProviders from 'test/utils/renderWithProviders';
 import '@testing-library/jest-dom';
 import {
@@ -7,8 +8,10 @@ import {
   mockGameState,
   mockPlayer,
 } from 'test/utils/mockStates';
+import * as redux from 'react-redux';
 
 import Pending from './Pending';
+import * as outgoingEvents from '../constants/outgoingEvents';
 
 describe('<Pending />', () => {
   it('renders without crashing', () => {
@@ -28,6 +31,31 @@ describe('<Pending />', () => {
         state: gameState,
       },
     });
-    expect(screen.getByText(playerName, { exact: false })).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(playerName))).toBeInTheDocument();
+  });
+  it('dispatches an action when pressing start', () => {
+    const playerName = 'onni';
+    const player = mockPlayer({ name: playerName });
+    const clientState = mockClientState({ playerName });
+    const gameState = mockGameState({}, [player]);
+
+    const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+    const mockDispatchFn = jest.fn();
+
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    renderWithProviders(<Pending />, {
+      preloadedState: {
+        player: clientState,
+        state: gameState,
+      },
+    });
+
+    userEvent.click(screen.getByText('Start'));
+    expect(useDispatchSpy).toHaveBeenCalledWith({
+      type: outgoingEvents.START,
+      roomName: 'room1',
+      initiator: playerName,
+    });
   });
 });
