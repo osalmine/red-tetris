45d757a263df5a8fc1829f20d805b49b1bfe9ac5
Onni Salminen <onni.salminen@wolt.com>
2023-04-24 22:23:09 +0300

    Client add more tests for Game

--- client/src/containers/Game.spec.tsx
+++ client/src/containers/Game.spec.tsx
@@ -18,7 +18,7 @@ describe('<Game />', () => {
   });
   it('renders board and next pieces', () => {
     const playerName = 'onni';
-    const player = mockPlayer({ name: playerName });
+    const player = mockPlayer({ name: playerName, state: 'playing' });
     const clientState = mockClientState({ playerName });
     const gameState = mockGameState({ roomState: 'playing' }, [player]);
     const pieceState = mockPieceState();
@@ -33,5 +33,67 @@ describe('<Game />', () => {
     expect(screen.getByText('Next pieces:')).toBeInTheDocument();
     expect(screen.getByTestId('0-0')).toBeInTheDocument();
     expect(screen.getByTestId('19-9')).toBeInTheDocument();
+  });
+  it('renders game ended if game is finished', () => {
+    const playerName = 'onni';
+    const player = mockPlayer({ name: playerName, state: 'finished' });
+    const clientState = mockClientState({ playerName });
+    const gameState = mockGameState({ roomState: 'playing' }, [player]);
+    const pieceState = mockPieceState();
+
+    renderWithProviders(<Game />, {
+      preloadedState: {
+        player: clientState,
+        state: gameState,
+        piece: pieceState,
+      },
+    });
+    expect(screen.getByText('Game Ended')).toBeInTheDocument();
+  });
+  it('renders opponent boards', () => {
+    const player1Name = 'onni';
+    const player1 = mockPlayer({ name: player1Name, state: 'playing' });
+    const clientState = mockClientState({ playerName: player1Name });
+    const player2Name = 'jussi';
+    const player2 = mockPlayer({ name: player2Name, state: 'playing' });
+    const gameState = mockGameState({ roomState: 'playing' }, [
+      player1,
+      player2,
+    ]);
+    const pieceState = mockPieceState();
+
+    renderWithProviders(<Game />, {
+      preloadedState: {
+        player: clientState,
+        state: gameState,
+        piece: pieceState,
+      },
+    });
+    expect(screen.getByText(player2Name)).toBeInTheDocument();
+    expect(screen.getByTestId('shadow-board-0-0')).toBeInTheDocument();
+    expect(screen.getByTestId('shadow-board-19-9')).toBeInTheDocument();
+  });
+  it('renders opponent game over', () => {
+    const player1Name = 'onni';
+    const player1 = mockPlayer({ name: player1Name, state: 'playing' });
+    const clientState = mockClientState({ playerName: player1Name });
+    const player2Name = 'jussi';
+    const player2 = mockPlayer({ name: player2Name, state: 'finished' });
+    const gameState = mockGameState({ roomState: 'playing' }, [
+      player1,
+      player2,
+    ]);
+    const pieceState = mockPieceState();
+
+    renderWithProviders(<Game />, {
+      preloadedState: {
+        player: clientState,
+        state: gameState,
+        piece: pieceState,
+      },
+    });
+    expect(screen.getByText('Game Over')).toBeInTheDocument();
+    expect(screen.getByTestId('shadow-board-0-0')).toBeInTheDocument();
+    expect(screen.getByTestId('shadow-board-19-9')).toBeInTheDocument();
   });
 });
