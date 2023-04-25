8f4cb4184b84f8542bf866c337549bac07927be9
Onni Salminen <onni.salminen@wolt.com>
2023-04-25 19:35:02 +0300

    Client add all tests for NextPiece

--- client/src/components/NextPiece.spec.tsx
+++ client/src/components/NextPiece.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithProviders from 'test/utils/renderWithProviders';
 
 import NextPiece from './NextPiece';
 
 describe('NextPiece component', () => {
   it('should render with pieceCharacter "O"', () => {
-    render(<NextPiece pieceCharacter="O" />);
+    renderWithProviders(<NextPiece pieceCharacter="O" />);
     expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
       'background-color: #313338'
     );
@@ -14,39 +15,72 @@ describe('NextPiece component', () => {
     );
   });
 
-  // it('should render with pieceCharacter "I"', () => {
-  //   render(<NextPiece pieceCharacter="I" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "I"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="I" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 
-  // it('should render with pieceCharacter "J"', () => {
-  //   render(<NextPiece pieceCharacter="J" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "J"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="J" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+    expect(screen.getByTestId('next-piece-2-1')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 
-  // it('should render with pieceCharacter "L"', () => {
-  //   render(<NextPiece pieceCharacter="L" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "L"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="L" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-2-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 
-  // it('should render with pieceCharacter "S"', () => {
-  //   render(<NextPiece pieceCharacter="S" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "S"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="S" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-2-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 
-  // it('should render with pieceCharacter "T"', () => {
-  //   render(<NextPiece pieceCharacter="T" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "T"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="T" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 
-  // it('should render with pieceCharacter "Z"', () => {
-  //   render(<NextPiece pieceCharacter="Z" />);
-  //   const cells = screen.getByTestId('next-piece-');
-  //   expect(cells).toHaveLength(16);
-  // });
+  it('should render with pieceCharacter "Z"', () => {
+    renderWithProviders(<NextPiece pieceCharacter="Z" />);
+    expect(screen.getByTestId('next-piece-0-1')).toHaveStyle(
+      'background-color: #313338'
+    );
+    expect(screen.getByTestId('next-piece-0-2')).toHaveStyle(
+      'background-color: #FFFFFF'
+    );
+  });
 });
