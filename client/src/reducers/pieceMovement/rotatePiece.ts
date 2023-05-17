ce1bf82916382407aba2708dbd2378bb3238cd93
Onni Salminen <onni.salminen@wolt.com>
2023-05-18 01:05:16 +0300

    Simplify piece rotating

--- client/src/reducers/pieceMovement/rotatePiece.ts
+++ client/src/reducers/pieceMovement/rotatePiece.ts
@@ -27,13 +27,6 @@ export const rotatePieceRight = ({ values, pieceType }: Piece) => {
     return values;
   }
 
-  const rotatedPiece = [...Array(values.length)].map(() => Array(values.length).fill(0));
-
-  for (let col = 0; col < values.length; col++) {
-    for (let row = 0; row < values[col].length; row++) {
-      rotatedPiece[col][row] = values[row][col];
-    }
-  }
-  const reversedRotatedPiece = rotatedPiece.map(row => row.reverse());
-  return reversedRotatedPiece;
+  const rotatedPiece = values[0].map((_, index) => values.map(row => row[index]).reverse());
+  return rotatedPiece;
 };
